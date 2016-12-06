2529c1dcfaadcbefb1a26070514f614f610503b1
Jordan Scholten <schojo13@gmail.com>
2016-12-05 20:39:30 -0800

    Added rudimentary db

---
+++ db/quotes.js
@@ -0,0 +1,8 @@
+var quotes = [
+  {
+    sifu: 'Uncle Iroh',
+    q: 'It is important to draw wisdom from different places. If you take it from only one place it become rigid and stale.'
+  }
+];
+
+module.exports = quotes;
--- server/router.js
+++ server/router.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var quotes = require('../db/quotes.js');
 var router = express.Router();
 
 router.get('/', function(req, res) {
   // build/delegateBuild of react webpage
   // send results as html to client on req
-  res.send('Welcome to Gotham');
+  res.send(quotes[0].q);
 });
 
 module.exports = router;
