56cc23d72079e2c0704f79498a3049a5b97fe607
Jordan Scholten <schojo13@gmail.com>
2016-12-05 16:50:18 -0800

    MVP: Quote displayed

---
+++ client/index.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+  <h1>hello everyone hi i want to succeed with MVP and I wish that you all love</h1>
+  <h2>3 person pep talk Andrew^2 and Alonzo</h2></div>,
+  document.getElementById('root')
+);
---
+++ compiled/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+ReactDOM.render(React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'hello everyone hi i want to succeed with MVP and I wish that you all love'
+  ),
+  React.createElement(
+    'h2',
+    null,
+    '3 person pep talk Andrew^2 and Alonzo'
+  )
+), document.getElementById('root'));
